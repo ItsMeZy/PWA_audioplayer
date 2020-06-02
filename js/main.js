5c278832e845019f1a9c74a413bca0258c4da4b4
Zyril Errol Gatchalian <jederrol@hotmail.it>
2020-06-02 15:56:11 +0200

    add animation play button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 import playUrl from '../icons/play.png';
 import pauseUrl from '../icons/pause.png';
+import anime from 'animejs/lib/anime.es';
+
+var playpause_anime = anime({
+  targets: '#playpause',
+  rotate:'1turn',
+  duration:200
+});
 
 var button_play_pause = document.getElementById("playpause");
 var player = document.getElementById("player");
 var isPlay = false;
 
 button_play_pause.onclick = function(){
+  playpause_anime.restart();
     if (isPlay){
         pauseAudio();
         button_play_pause.style.backgroundImage = "url("+playUrl+")";
@@ -55,11 +63,46 @@ var startTimer = function(duration, element){
     timer = setTimeout(function (){advance(duration, element)}, 100);
   }
 }
-
-const worker = new Worker('./worker.js');
 const canvas = document.querySelector('canvas');
+console.log("ciao");
+window.onchange = function(){
+  var audioCtx = new AudioContext()
+  var analyser = audioCtx.createAnalyser();
+  let source = audioCtx.createMediaElementSource(player);
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
+  
+  analyser.fftSize = 2048;
+  
+  const bufferLenght = analyser.frequencyBinCount;
+  
+  const dataArray = new Uint8Array(bufferLenght);
+  
+  console.log('DATA-ARRAY',dataArray);
+  
+  const barWidth = (cWidth / bufferLenght)*13;
+  
+  let barHeight;
+  let x=0;
+  
+  function renderFrame(){
+    requestAnimationFrame(renderFrame);
+  
+    x=0;
+  
+    analyser.getByteFrequencyData(dataArray);
+  
+    ctx.fillStyle = "rgba(0,0,0,0.2)";
+    ctx.fillRect(0,0, cWidth,cHeight);
+  }
+
+//const worker = new Worker('./worker.js');
+
+const ctx = canvas.getContext("2d");
 const cHeight = canvas.height;
 const cWidth = canvas.width;
 
-const offscreenCanvas = canvas.transferControlToOffscreen(); 
-worker.postMessage({ offscreenCanvas }, [offscreenCanvas]);
+//const offscreenCanvas = canvas.transferControlToOffscreen(); 
+//worker.postMessage({ offscreenCanvas }, [offscreenCanvas]);
+}
+  
